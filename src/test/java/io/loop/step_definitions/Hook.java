8d23a54e6bc6ed0b69270a1f29e796821dc0d13b
LenkaScott <helene_ctr@hotmail.com>
2025-02-16 16:02:51 -0800

    day_12

--- src/test/java/io/loop/step_definitions/Hook.java
+++ src/test/java/io/loop/step_definitions/Hook.java
@@ -1,17 +1,34 @@
 package io.loop.step_definitions;
 
 import io.cucumber.java.*;
+import io.loop.utilities.BrowserUtils;
 import io.loop.utilities.Driver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static io.loop.utilities.BrowserUtils.myScenario;
 
 public class Hook {
     @Before
-    public void SetUp() {
+    public void SetUp(Scenario scenario) {
         Driver.getDriver();
+        myScenario = scenario;
 
 
     }
     @After
-    public void TearDown() {
+    public void TearDown(Scenario scenario) {
+        // ONLY TAKES SCREENSHOT WHEN SCENARIO IS FAILED
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            myScenario.attach(screenshot, "image/png", myScenario.getName());
+        }
         Driver.closeDriver();
+    }
+    @AfterStep
+    public void takeScreenshot(Scenario scenario) {
+        final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        myScenario.attach(screenshot, "image/png", myScenario.getName());
+
     }
 }
