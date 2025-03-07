38aec58e0ad023b277c1d64a50d1dbe76fb07184
LenkaScott <helene_ctr@hotmail.com>
2025-03-06 16:03:09 -0800

    day_16

--- src/test/java/io/loop/runner/CukesRunner.java
+++ src/test/java/io/loop/runner/CukesRunner.java
@@ -7,12 +7,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/html-reports/cucumber-report.html",  "json:target/json-reports/json-report.json"},
+        plugin = {"pretty",
+                "rerun:target/rerun.txt",
+                "html:target/html-reports/cucumber-report.html",  "json:target/json-reports/json-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/resources/features",
         glue = "io/loop/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = "@xray",
-        monochrome = true
+        monochrome = true,
+        publish = false
 )
 
 public class CukesRunner {
